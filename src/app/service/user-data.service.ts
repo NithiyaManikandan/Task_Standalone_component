8853476c294ec541c6d90250ff9bfcd3250d8ed8
NithiyaManikandan <nithiyasrimanikandan@gmail.com>
2023-03-27 11:39:48 +0530

    env

--- src/app/service/user-data.service.ts
+++ src/app/service/user-data.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { map } from 'rxjs';
 import { User } from '../model/user_model';
-
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,25 +11,25 @@ export class UserDataService {
   constructor(private http: HttpClient) {}
 
   getAllUser() {
-    return this.http.get<User[]>(`http://localhost:3000/user`);
+    return this.http.get<User[]>(environment.apiUrl + `/user`);
   }
   getColumn() {
-    return this.http.get<string[]>(`http://localhost:3000/column`);
+    return this.http.get<string[]>(environment.apiUrl + `/column`);
   }
   getUserById(id: number) {
-    return this.http.get<User[]>(`http://localhost:3000/user/${id}`);
+    return this.http.get<User[]>(environment.apiUrl + `/user/${id}`);
   }
   updateUserData(data: any) {
-    return this.http.put<User[]>(`http://localhost:3000/user/${data.id}`, data);
+    return this.http.put<User[]>(environment.apiUrl + `/user/${data.id}`, data);
   }
   deleteUser(id: number) {
-    return this.http.delete<User[]>(`http://localhost:3000/user/${id}`);
+    return this.http.delete<User[]>(environment.apiUrl + `/user/${id}`);
   }
   postUserData(data: any) {
-    return this.http.post<User[]>(`http://localhost:3000/user`, data);
+    return this.http.post<User[]>(environment.apiUrl + `/user`, data);
   }
   checkId(id: number) {
-    return this.http.get<User[]>(`http://localhost:3000/user`).pipe(
+    return this.http.get<User[]>(environment.apiUrl + `/user`).pipe(
       map((res) => {
         return res.find((ele) => ele.id == id);
       })
